aae3ba017216b6f1861303c3a976e789c861111b
llievre <loic.lievre@heig-vd.ch>
2021-03-22 15:55:05 +0100

    step 2 ok

--- files/manual-encryption.py
+++ files/manual-encryption.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""" Manually decrypt a wep message given the WEP key"""
+""" Manually encrypt a wep message given the WEP key"""
 
 __author__      = "Schranz Guillaume & Lièvre Loïc"
 __copyright__   = "Copyright 2021, HEIG-VD"
@@ -15,12 +15,29 @@ import binascii
 from rc4 import RC4
 
 #Cle wep AA:AA:AA:AA:AA
-key= b'\xaa\xaa\xaa\xaa\xaa'
+key = b'\xaa\xaa\xaa\xaa\xaa'
 
-text = "aaaa03000000080600010800060400019027e4ea61f2c0a80164000000000000c0a801c8"
+#reprend la trame du fichier pour la modifier
+arp = rdpcap('arp.cap')[0]
+
+#on assigne des valeurx hexa au texte
+text = bytes.fromhex("aaaa03000000080600010800060400019027e4ea61f2c0a80164000000000000c0a801c8")
+
+#seed avec l'iv contenu dans le modele de trame et la clé définie
+#nous aurions pu definir un autre iv selon les infos du cours en slide 25 du pdf WEP
+seed = arp.iv+key
+
+#calcul de l'icv avec le crc32
+icv = binascii.crc32(text).to_bytes(4, byteorder='little')
 
 # chiffrement rc4
 cipher = RC4(seed, streaming=False)
-encryptedText=cipher.crypt(message_encrypted)
+encryptedText = cipher.crypt(text + icv)
+ 
+#on complete la trame avec les wepdata et l'icv calculé
+arp.wepdata = encryptedText[:-4]
+icv_enclair = encryptedText[-4:]
+arp.icv = struct.unpack('!L', icv_enclair)[0]
 
-print(encryptedText)
+#écris le fichier
+wrpcap("step2.pcap", arp)
